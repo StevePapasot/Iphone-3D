547e0a25f7d139aeadc70c76c57af916717d1a89
StevenPapasot <75527414+StevePapasot@users.noreply.github.com>
2023-12-30 13:13:46 +0200

    App.jsx update

--- App.jsx
+++ App.jsx
@@ -1,17 +1,17 @@
 import Nav from "../src/components/Nav";
 import Jumbotron from "./components/Jumbotron";
-import SoundSection from '../src/components/SoundSection';
+import SoundSection from "../src/components/SoundSection";
 import DisplaySection from "../src/components/DisplaySection";
-
+import WebgiViewer from "../src/components/WebgiViewer";
 
 function App() {
-
   return (
     <div className="App">
-        <Nav />
-        <Jumbotron />
-        <SoundSection />
-        <DisplaySection />
+      <Nav />
+      <Jumbotron />
+      <SoundSection />
+      <DisplaySection />
+      <WebgiViewer />
     </div>
   );
 }
